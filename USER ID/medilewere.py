9c03ea62dcfcaa6ea1f82e68d801ad26054dce8c
python-hacked <satishchoudhary394@gmail.com>
2023-07-22 21:50:45 +0530

    add custom

---
+++ USER ID/medilewere.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, name):
+        name = self.name
+        print(f"Hello {name}!")
+
+class Persone:
+    def __init__(self,age):
+        age = self.age
+        print(f"Hello {age}!")
+
--- USER ID/myemail/id/views.py
+++ USER ID/myemail/id/views.py
@@ -33,87 +33,90 @@ from id.models import Person
 
 # THIS IS USE FOR SIGNAL ONLY
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
+            username = form.cleaned_data.get("username")
             messages.success(
-                request, f'Your account has been created! You are now able to log in')
-            return redirect('login')
+                request, f"Your account has been created! You are now able to log in"
+            )
+            return redirect("login")
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, "users/register.html", {"form": form})
 
 
 @login_required
 def profile(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.user.profile)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has been updated!')
-            return redirect('profile')
+            messages.success(request, f"Your account has been updated!")
+            return redirect("profile")
 
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
 
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
+    context = {"u_form": u_form, "p_form": p_form}
 
-    return render(request, 'users/profile.html', context)
+    return render(request, "users/profile.html", context)
+
+
 # END OF THE SIGNAL
 
 
 # Create your views here.
 
+
 def view(request):
-    render('home.html')
+    render("home.html")
 
 
 def login_page(request):
-    return render(request, 'id/login.html')
+    return render(request, "id/login.html")
 
 
 def welcome_page(request):
-    return render(request, 'id/welcome.html')
+    return render(request, "id/welcome.html")
 
 
 def index(request):
-    return render(request, 'id/home.html', context={})
+    return render(request, "id/home.html", context={})
 
 
 def home(request):
-    return render(request, 'id/login.html', context={})
+    return render(request, "id/login.html", context={})
 
 
 def successful(request):
-    return render(request, 'id/welcome.html', context={})
+    return render(request, "id/welcome.html", context={})
+
 
 # THIS IS ONLY FOR STARTING Response FOR ALL PAGES
 
 
 def home(request):
-    return ("this is my first page.")
+    return "this is my first page."
 
 
 def login(request):
-    return ("this is my first page.")
+    return "this is my first page."
 
 
 def welcome(request):
-    return ("this is my first page.")
+    return "this is my first page."
 
 
 def dataon(request):
-    return ("this is my first page.")
+    return "this is my first page."
+
 
 # THIS IS ALL DATA VIEW
 
@@ -140,59 +143,68 @@ class dataus(View):
 
 #  create Ragistration form
 def form_data(request):
-    if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        Company_name = request.POST['company']
-        Email_name = request.POST['Email']
-        Phone_number = request.POST['Phone']
-        Password = make_password(request.POST['Password'])
+    if request.method == "POST":
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        Company_name = request.POST["company"]
+        Email_name = request.POST["Email"]
+        Phone_number = request.POST["Phone"]
+        Password = make_password(request.POST["Password"])
         if Person.objects.filter(Phone_number=Phone_number).exists():
             messages.error(request, "phone number already exists")
-            return redirect('/')
+            return redirect("/")
 
         elif Person.objects.filter(Email_name=Email_name).exists():
             messages.error(request, "Email id already exists")
-            return redirect('/')
+            return redirect("/")
 
         else:
-            Person.objects.create(first_name=first_name,
-                                  last_name=last_name, Company_name=Company_name,
-                                  Email_name=Email_name, Phone_number=Phone_number, Password=Password)
-            return redirect('/login/')
+            Person.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                Company_name=Company_name,
+                Email_name=Email_name,
+                Phone_number=Phone_number,
+                Password=Password,
+            )
+            return redirect("/login/")
 
 
 # create login form
 def Login_form(request):
-    if request.method == 'POST':
-        Phone_number = request.POST['Phone']
-        User_Password = request.POST['Password']
+    if request.method == "POST":
+        Phone_number = request.POST["Phone"]
+        User_Password = request.POST["Password"]
         if Person.objects.filter(Phone_number=Phone_number).exists():
             obj = Person.objects.get(Phone_number=Phone_number)
             Password = obj.Password
             if check_password(User_Password, Password):
-                return redirect('/welcome/')
+                return redirect("/welcome/")
             else:
-                return HttpResponse('password incorrect')
+                return HttpResponse("password incorrect")
         else:
-            return HttpResponse('phone number is not registered')
+            return HttpResponse("phone number is not registered")
 
 
 # THIS IS CLIENT Authentication SYSTEM
 # create Table data form
 def data(request):
-    persons = Person.objects.filter(is_active=True).order_by('id')
+    persons = Person.objects.filter(is_active=True).order_by("id")
 
-    return render(request, 'id/table.html', context={
-        'request': request,
-        'persons': persons,
-    })
+    return render(
+        request,
+        "id/table.html",
+        context={
+            "request": request,
+            "persons": persons,
+        },
+    )
 
 
 # create delete button
 def delete_user(request):
-    if request.method == 'POST':
-        data = request.body.decode('utf-8')
+    if request.method == "POST":
+        data = request.body.decode("utf-8")
         uid = json.loads(data)
         if Person.objects.filter(id=uid).exists():
             Person.objects.filter(id=uid).update(is_active=False)
@@ -206,26 +218,33 @@ def delete_user(request):
 # create Edit button
 def update_view(request, uid):
     res = Person.objects.get(id=uid)
-    return render(request, 'id/update.html', context={
-
-        'person': res,
-    })
+    return render(
+        request,
+        "id/update.html",
+        context={
+            "person": res,
+        },
+    )
 
 
 # USE UPDATE DATA
 def update_form_data(request):
-    if request.method == 'POST':
-        uid = request.POST['uid']
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        Company_name = request.POST['company']
-        Email_name = request.POST['Email']
-        Phone_number = request.POST['Phone']
+    if request.method == "POST":
+        uid = request.POST["uid"]
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        Company_name = request.POST["company"]
+        Email_name = request.POST["Email"]
+        Phone_number = request.POST["Phone"]
 
-        Person.objects.filter(id=uid).update(first_name=first_name,
-                                             last_name=last_name, Company_name=Company_name,
-                                             Email_name=Email_name, Phone_number=Phone_number)
-        return redirect('/data/')
+        Person.objects.filter(id=uid).update(
+            first_name=first_name,
+            last_name=last_name,
+            Company_name=Company_name,
+            Email_name=Email_name,
+            Phone_number=Phone_number,
+        )
+        return redirect("/data/")
         # redirect to table form    AND OF THE CLIENT TABLE
 
 
@@ -238,10 +257,14 @@ class RegisterAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        return Response({
-            "users": UserSerializer(user, context=self.get_serializer_context()).data,
-            "token": AuthToken.objects.create(user)[1]
-        })
+        return Response(
+            {
+                "users": UserSerializer(
+                    user, context=self.get_serializer_context()
+                ).data,
+                "token": AuthToken.objects.create(user)[1],
+            }
+        )
 
 
 # LOGIN API
@@ -251,7 +274,7 @@ class LoginAPI(KnoxLoginView):
     def post(self, request, format=None):
         serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        users = serializer.validated_data['users']
+        users = serializer.validated_data["users"]
         # login(users)
         return super(LoginAPI, self).post(request, format=None)
 
@@ -260,6 +283,7 @@ class ChangePasswordView(generics.UpdateAPIView):
     """
     An endpoint for changing password.
     """
+
     serializer_class = ChangePasswordSerializer
     model = User
     permission_classes = (IsAuthenticated,)
@@ -275,15 +299,18 @@ class ChangePasswordView(generics.UpdateAPIView):
         if serializer.is_valid():
             # Check old password
             if not self.object.check_password(serializer.data.get("old_password")):
-                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"old_password": ["Wrong password."]},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
             # set_password also hashes the password that the user will get
             self.object.set_password(serializer.data.get("new_password"))
             self.object.save()
             response = {
-                'status': 'success',
-                'code': status.HTTP_200_OK,
-                'message': 'Password updated successfully',
-                'data': []
+                "status": "success",
+                "code": status.HTTP_200_OK,
+                "message": "Password updated successfully",
+                "data": [],
             }
 
             return Response(response)
